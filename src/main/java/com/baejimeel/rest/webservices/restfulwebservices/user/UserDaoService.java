fe7120953d577b75992f7dd382da03f6dc0ec1a9
baejimil <qowhdgus629@naver.com>
2023-10-12 16:42:47 +0900

    internationalized complete

--- src/main/java/com/baejimeel/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/baejimeel/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -1,2 +1,42 @@
-package com.baejimeel.rest.webservices.restfulwebservices.user;public class UserDaoService {
+package com.baejimeel.rest.webservices.restfulwebservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 0;
+
+    static {
+        users.add(new User(++usersCount, "bae", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "cho", LocalDate.now().minusYears(21)));
+        users.add(new User(++usersCount, "kim", LocalDate.now().minusYears(3)));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
 }
